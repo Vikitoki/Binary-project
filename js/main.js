ae79ed025b11b082f17f9c314d61c49080b9c310
Vasily <vasa-svistun@mail.ru>
2021-02-11 13:04:43 +0300

    Исправлены пути в файлах

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-$((function(){$("[data-filter]").on("click",(function(t){t.preventDefault();let a=$(this).data("filter");$("[data-cat]").animate({opacity:"0"},200),"all"===a?setTimeout((function(){$("[data-cat]").css({display:"block",opacity:"1"})}),300):$("[data-cat]").each((function(){let t=$(this).data("cat");t!=a?($(this).animate({opacity:"0"},200),setTimeout((function(){$(`[data-cat = '${t}']`).css("display","none")}),300)):(setTimeout((function(){$(`[data-cat = '${t}']`).css("display","block")}),300),$(this).animate({opacity:"1"},200))})),$("[data-filter]").removeClass("active"),$(this).addClass("active")}));const t=$(".intro__btn");$(t).on("click",(function(t){t.preventDefault();let a=$(this).data("menu"),i=$(a).offset().top;$("html,body").animate({scrollTop:i},700)})),$(window).on("scroll",(function(){let t=$(this).scrollTop()/3;console.log(t),$("#intro").css("background-position",`0 -${t}px`)}))}));
+$(function () {
+  $("[data-filter]").on("click", function (t) {
+    t.preventDefault();
+    let a = $(this).data("filter");
+    $("[data-cat]").animate({ opacity: "0" }, 200),
+      "all" === a
+        ? setTimeout(function () {
+            $("[data-cat]").css({ display: "block", opacity: "1" });
+          }, 300)
+        : $("[data-cat]").each(function () {
+            let t = $(this).data("cat");
+            t != a
+              ? ($(this).animate({ opacity: "0" }, 200),
+                setTimeout(function () {
+                  $(`[data-cat = '${t}']`).css("display", "none");
+                }, 300))
+              : (setTimeout(function () {
+                  $(`[data-cat = '${t}']`).css("display", "block");
+                }, 300),
+                $(this).animate({ opacity: "1" }, 200));
+          }),
+      $("[data-filter]").removeClass("active"),
+      $(this).addClass("active");
+  });
+  const t = $(".intro__btn");
+  $(t).on("click", function (t) {
+    t.preventDefault();
+    let a = $(this).data("menu"),
+      i = $(a).offset().top;
+    $("html,body").animate({ scrollTop: i }, 700);
+  }),
+    $(window).on("scroll", function () {
+      let t = $(this).scrollTop() / 3;
+      console.log(t), $("#intro").css("background-position", `0 -${t}px`);
+    });
+});
